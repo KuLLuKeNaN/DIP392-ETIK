2165aaacf5d373a01bffcb4442d99a84b857c56b
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-20 15:53:20 +0300

    changes

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,29 @@
-// src/server/app.js
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
-// Basit route
+// Veritabanına bağlan
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+// Root test endpoint
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-module.exports = app; 
+module.exports = app;
