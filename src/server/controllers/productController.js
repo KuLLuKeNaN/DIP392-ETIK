cbd22333e5b9103401c5d85cb32b54b2d928b2a2
KuLLuKeNaN <kayraakel@gmail.com>
2025-05-20 20:50:37 +0300

    change

--- src/server/controllers/productController.js
+++ src/server/controllers/productController.js
@@ -42,8 +42,15 @@ const createProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).json({ message: 'Product not found' });
+
+    if (product.owner.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Not authorized to update this product' });
+    }
+
     const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updated) return res.status(404).json({ message: 'Ürün bulunamadı' });
     res.json(updated);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -52,13 +59,21 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
   try {
-    const deleted = await Product.findByIdAndDelete(req.params.id);
-    if (!deleted) return res.status(404).json({ message: 'Ürün bulunamadı' });
-    res.json({ message: 'Ürün silindi' });
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).json({ message: 'Product not found' });
+
+    if (product.owner.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Not authorized to delete this product' });
+    }
+
+    await product.remove();
+    res.json({ message: 'Product deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
 
 module.exports = {
   getAllProducts,
