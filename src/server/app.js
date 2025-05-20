cac5b14006e72a2040ad59d355643b5c77f5994e
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-20 15:46:11 +0300

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,11 @@
+// src/server/app.js
 const express = require('express');
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Basit route
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
