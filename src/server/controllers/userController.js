f026b3e69be85e8661c0fb19696bcda40ad1aa10
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-25 16:49:30 +0300

    Update userController.js

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -3,25 +3,32 @@ const bcrypt = require('bcryptjs');
 
 const updateProfile = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, surname, email, phone, birthday, password } = req.body;
 
     const user = await User.findById(req.user._id);
     if (!user) return res.status(404).json({ message: 'User not found' });
 
     if (name) user.name = name;
+    if (surname) user.surname = surname;
     if (email) user.email = email;
+    if (phone) user.phone = phone;
+    if (birthday) user.birthday = birthday;
     if (password) {
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
     }
 
     const updatedUser = await user.save();
+
     res.json({
       message: 'Profile updated successfully',
       user: {
         _id: updatedUser._id,
         name: updatedUser.name,
-        email: updatedUser.email
+        surname: updatedUser.surname,
+        email: updatedUser.email,
+        phone: updatedUser.phone,
+        birthday: updatedUser.birthday
       }
     });
   } catch (err) {
