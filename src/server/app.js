4110117e5d96640d24086a22980d3a037f7a7789
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-26 23:48:14 +0300

    changes

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
