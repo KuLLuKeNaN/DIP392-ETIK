8b64e9da212ba893402dbeaec17153500de5e3de
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-27 03:04:07 +0300

    changes

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -36,7 +36,15 @@ const updateProfile = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
-
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select('-password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 module.exports = {
   updateProfile
 };
--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { updateProfile } = require('../controllers/userController');
 const verifyToken = require('../middleware/verifyToken');
+const verifyToken = require('../middleware/verifyToken');
+const { getUserProfile } = require('../controllers/userController');
 
+router.get('/me', verifyToken, getUserProfile);
 // Kullanıcı profilini güncelle
 router.put('/me', verifyToken, updateProfile);
 
