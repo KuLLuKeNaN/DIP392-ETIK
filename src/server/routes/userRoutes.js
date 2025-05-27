d20d5a38b6fc9c5a7d80c822a2dc288076f17258
KuLLuKeNaN <118316511+KuLLuKeNaN@users.noreply.github.com>
2025-05-27 03:07:11 +0300

    Update userRoutes.js

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { updateProfile } = require('../controllers/userController');
 const verifyToken = require('../middleware/verifyToken');
+const { updateProfile, getUserProfile } = require('../controllers/userController');
 
-const { getUserProfile } = require('../controllers/userController');
-
+// Kullanıcının bilgilerini getir
 router.get('/me', verifyToken, getUserProfile);
-// Kullanıcı profilini güncelle
+
+// Kullanıcının bilgilerini güncelle
 router.put('/me', verifyToken, updateProfile);
 
 module.exports = router;
